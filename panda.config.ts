d1787d3407c98140748ad7cb56ed19a2c3ca29cc
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-11-22 17:34:37 +0100

    added colors to panda config

--- panda.config.ts
+++ panda.config.ts
@@ -16,7 +16,75 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          white: { value: '#fff' },
+          purple: {
+            200: { value: '#C75AF6' },
+            500: { value: '#ad1fea' },
+          },
+          skyBlue: {
+            400: { value: '#62BCFA' },
+          },
+          blue: {
+            200: { value: '#7C91F9' },
+            500: { value: '#4661e6' },
+            800: { value: '#373F68' },
+          },
+          grey: {
+            50: { value: '#f7f8fd' },
+            100: { value: '#f2f4ff' },
+            300: { value: '#F7F8FD' },
+          },
+          blueGrey: {
+            700: { value: '#647196' },
+            800: { value: '#3A4374' },
+          },
+          peach: {
+            500: { value: '#F49F85' },
+          },
+          red: {
+            200: { value: '#E98888' },
+            500: { value: '#D73737' },
+          },
+        },
+      },
+      semanticTokens: {
+        colors: {
+          primary: { value: { base: '{colors.purple.500}' } },
+          primaryAlt: { value: { base: '{colors.purple.200}' } },
+          secondary: { value: { base: '{colors.blue.200}' } },
+          secondaryAlt: { value: { base: '{colors.blue.500}' } },
+          planned: { value: { base: '{colors.peach.500}' } },
+          inProgress: { value: { base: '{colors.purple.500}' } },
+          live: { value: { base: '{colors.skyBlue.400}' } },
+          danger: { value: { base: '{colors.red.500}' } },
+          dangerAlt: { value: { base: '{colors.red.200}' } },
+          background: {
+            DEFAULT: { value: { base: '{colors.grey.300}' } },
+            header: { value: { base: '{colors.blue.800}' } },
+            formField: {
+              value: { base: '{colors.grey.300}' },
+            },
+          },
+          button: {
+            background: {
+              neutral: { value: { base: '{colors.grey.100}' } },
+              neutralAlt: { value: { base: '{colors.grey.50}' } },
+            },
+            text: {
+              DEFAULT: { value: { base: '{colors.bluegrey.800}' } },
+              accent: { value: { base: '{colors.blue.500}' } },
+            },
+          },
+          text: {
+            DEFAULT: { value: { base: '{colors.bluegrey.700}' } },
+            heading: { value: { base: '{colors.bluegrey.800}' } },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
