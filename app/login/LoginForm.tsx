57db8dbf365bce5d596daefbac165ae98cdae4b7
cpatigny <63152989+cpatigny@users.noreply.github.com>
2023-11-26 18:29:11 +0100

    extract login and sign up forms from page files

---
+++ app/login/LoginForm.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { ROUTES } from '@/routes';
+import { css } from '@/styled-system/css';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const inputStyles = css({
+  display: 'block',
+  border: '1px solid black',
+});
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const signInData = await signIn('credentials', {
+      redirect: false,
+      email,
+      password,
+    });
+
+    if (signInData?.error) {
+      console.log(signInData.error);
+      return;
+    }
+
+    router.push(ROUTES.ADMIN);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          name='email'
+          type='text'
+          className={inputStyles}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor='password'>Password</label>
+        <input
+          id='password'
+          name='password'
+          type='password'
+          className={inputStyles}
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <button className={css({ bg: 'primary' })}>Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
---
+++ app/sign-up/SignUpForm.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { ROUTES } from '@/routes';
+import { css } from '@/styled-system/css';
+import { useRouter } from 'next/navigation';
+import { createUser } from './actions';
+
+const inputStyles = css({
+  display: 'block',
+  border: '1px solid black',
+});
+
+function SignUpForm() {
+  const router = useRouter();
+
+  const handleSubmit = async (formData: FormData) => {
+    const response = await createUser(formData);
+    if (response.error) {
+      return;
+    }
+
+    // replace this line by a request to sign in in order to auto sign in after sign up
+    router.push(ROUTES.LOGIN);
+  };
+
+  return (
+    <form action={handleSubmit}>
+      <div>
+        <label htmlFor='username'>Username</label>
+        <input
+          id='username'
+          name='username'
+          type='text'
+          className={inputStyles}
+          aria-required='true'
+        />
+      </div>
+      <div>
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          className={inputStyles}
+          aria-required='true'
+        />
+      </div>
+      <div>
+        <label htmlFor='password'>Password</label>
+        <input
+          id='password'
+          name='password'
+          type='password'
+          className={inputStyles}
+          aria-required='true'
+        />
+      </div>
+      <button className={css({ bg: 'primary' })}>Sign Up</button>
+    </form>
+  );
+}
+
+export default SignUpForm;
